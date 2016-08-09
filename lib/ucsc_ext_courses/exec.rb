38b57d03dc11a42d848ba04f46285b45d19a7821
poc <poc7667@gmail.com>
2016-08-08 22:10:14 -0700

    add full certificate programs

--- lib/ucsc_ext_courses/exec.rb
+++ lib/ucsc_ext_courses/exec.rb
@@ -18,12 +18,47 @@ OFFERING_API = "http://course.ucsc-extension.edu/modules/shop/searchOfferings.ac
 DETAIL_API  = "http://course.ucsc-extension.edu/modules/shop/defaultSections.action"
 FULL_SCHEDULE_API = "http://course.ucsc-extension.edu/modules/shop/sectionSchedule.action"
 
-COURSES = {
-  "computer_programming": 80,
-  "db_and_data_analytics": 83,
-  "internet_programming_and_development": 95
-}
-# curl ''  --data '&CatalogID=80&startPosition=0&pageSize=100' --compressed
+COURSES = [
+  {id:"81", certificate_name:"Account Course Catalog"},
+  {id:"82", certificate_name:"Accounting CPA Course Catalog"},
+  {id:"85", certificate_name:"Administrative and Executive Assistants Course Catalog"},
+  {id:"87", certificate_name:"Bioinformatics Course Catalog"},
+  {id:"93", certificate_name:"Biotechnology Course Catalog"},
+  {id:"89", certificate_name:"Business Administration Course Catalog"},
+  {id:"86", certificate_name:"Certified Bookkeeping Course Catalog"},
+  {id:"79", certificate_name:"Clinical Trials Course Catalog"},
+  {id:"80", certificate_name:"Computer Programming Course Catalog"},
+  {id:"97", certificate_name:"Credential Courses Catalog"},
+  {id:"83", certificate_name:"Database and Data Analytics Course Catalog"},
+  {id:"99", certificate_name:"Early Childhood Education Course Catalog"},
+  {id:"113", certificate_name:"Early Childhood Education: Supervision &amp; Administration Course Catalog"},
+  {id:"114", certificate_name:"Educational Therapy Course Catalog"},
+  {id:"84", certificate_name:"Environmental Health and Safety Management Course Catalog"},
+  {id:"88", certificate_name:"Embedded Systems Course Catalog"},
+  {id:"91", certificate_name:"Personal Financial Planning Course Catalog"},
+  {id:"111", certificate_name:"General Interest Science Course Catalog"},
+  {id:"94", certificate_name:"Human Resources Management Course Catalog"},
+  {id:"241", certificate_name:"Information Technology Course Catalog"},
+  {id:"115", certificate_name:"Instructional Design and Delivery Course Catalog"},
+  {id:"95", certificate_name:"Internet Programming and Development Course Catalog"},
+  {id:"101", certificate_name:"Legal Studies Course Catalog"},
+  {id:"98", certificate_name:"Linux Programming and Administration Course Catalog"},
+  {id:"102", certificate_name:"Marketing Course Catalog"},
+  {id:"110", certificate_name:"MBA Prerequisite Course Catalog"},
+  {id:"104", certificate_name:"MCLE courses"},
+  {id:"112", certificate_name:"Medical Devices Course Catalog"},
+  {id:"322", certificate_name:"Mobile Application Development Course Catalog (Professional Award)"},
+  {id:"344", certificate_name:"Pre-Health Professions"},
+  {id:"105", certificate_name:"Project and Program Management Course Catalog"},
+  {id:"78", certificate_name:"Regulatory Affairs Course Catalog"},
+  {id:"107", certificate_name:"Software Engineering and Quality Course Catalog"},
+  {id:"290", certificate_name:"List of special offerings"},
+  {id:"108", certificate_name:"Technical Writing Course Catalog"},
+  {id:"117", certificate_name:"Teaching English to Speakers of Other Languages Course Catalog"},
+  {id:"118", certificate_name:"VLSI Engineering Course Catalog"},
+  {id:"119", certificate_name:"Web and Interactive Media Design Course Catalog"},
+]
+# curl ''  --data '&id=80&startPosition=0&pageSize=100' --compressed
 module UcscExtCourses
   module Exec
 
@@ -34,9 +69,11 @@ module UcscExtCourses
       end
 
       def run
-        COURSES.each do |cate_name, cate_id|
+        COURSES.each do |certificate|
+          cate_name  = certificate[:certificate_name]
+          cate_id = certificate[:id]
           @cate_name = cate_name
-          http = Curl.get(OFFERING_API, {:CatalogID => cate_id, :startPosition => 0})
+          http = Curl.get(OFFERING_API, {:id => cate_id, :startPosition => 0})
           doc = Nokogiri::HTML.parse(http.body_str)
           offering_and_section_ids = get_offering_and_section_ids(doc)
           fetch_courses(offering_and_section_ids)
